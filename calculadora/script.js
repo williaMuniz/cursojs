e407734909fa993f00abb989610aa0cff4da03ac
williaMuniz <wfmuniz18@gmail.com>
2023-09-14 18:05:37 -0300

    atualização

--- calculadora/script.js
+++ calculadora/script.js
@@ -8,14 +8,22 @@ function calcular() {
     let p = Number(pass.value)
     if (i == 0 && f == 0 && p == 0) {
         res.innerHTML = `Por favor, insira os valores nos campos para realizarmos a contagem.`
-    } else if (i == 0 || f == 0 || p == 0){
+    } else if (i == 0 || f == 0){
         res.innerHTML = `[error] Um ou mais valores esta faltando`
     } else {
-        
-
-
-
-        
+        if (p <= 0) {
+            window.alert('Passo Invalido! Considerando passo com valor 1')
+            p = 1
+        }
+        if (i < f){ //contagem crescente
+            for (let c=i; c<=f; c+=p) { //c+=p contador recebe ele mesmo mais o passo.
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        } else { //contagem regressiva
+            for (let c=i; c>=f; c-=p) { // C recebe i, contador recebe ele msm e tira o passo
+                res.innerHTML += ` ${c} \u{1F449}` 
+            }
+        }
+        res.innerHTML += `\u{1F3c1}`
     }
-
 }
